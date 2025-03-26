99ea3953410c99d76afd1d27a138c60344de5a31
DeeBoe <154568361+deezboe@users.noreply.github.com>
2025-03-26 18:40:37 -0400

    Update filemanager.js

--- File-Explorer/filemanager.js
+++ File-Explorer/filemanager.js
@@ -1,98 +1,37 @@
-// Replace the existing component with this modified version
-const FileManagerComponent = function (e) {
-  Object(f.a)(n, e);
-  var t = Object(m.a)(n);
-  function n(e) {
-    var i;
-    return Object(d.a)(this, n), 
-    (i = t.call(this, e)).render = function () {
-      var e = Object(u.a)(Object(u.a)({}, g.a.apiOptions), {}, { apiRoot: "/" }),
-          t = "Loading...";
-      return i.state.isReady && (t = Object(a.jsx)("div", { 
-        style: { height: "480px" }, 
-        children: Object(a.jsx)(p.FileManager, { 
-          children: Object(a.jsx)(p.FileNavigator, { 
-            id: "filemanager-1", 
-            api: g.a.api, 
-            apiOptions: e, 
-            capabilities: g.a.capabilities, 
-            listViewLayout: g.a.listViewLayout, 
-            viewLayoutOptions: g.a.viewLayoutOptions 
-          }) 
-        }) 
-      })), 
-      Object(a.jsx)("div", { className: "App", children: t })
-    }, 
-    i.state = { isReady: !1 }, 
-    i.componentDidMount = i.componentDidMount.bind(Object(c.a)(i)), 
-    i
-  }
-  return Object(s.a)(n, [{
-    key: "componentDidMount",
-    value: function () {
-      var e = this;
-      // Directly initialize IndexedDB without AsyncMirror
-      const request = indexedDB.open('file_sandbox', 1);
-      
-      request.onupgradeneeded = function(event) {
-        const db = event.target.result;
-        if (!db.objectStoreNames.contains('files')) {
-          db.createObjectStore('files', { keyPath: 'id' });
+// In the componentDidMount or initialization section
+componentDidMount() {
+    var e = this;
+    h.configure({
+        fs: "AsyncMirror",
+        options: {
+            sync: { fs: "InMemory" },
+            async: { fs: "IndexedDB", options: { storeName: "file_sandbox" } }
         }
-      };
-      
-      request.onsuccess = function() {
-        window.db = request.result;
+    }, function(t) {
+        if (t) throw t;
+        
+        // Add IndexedDB polyfill if needed
+        if (!window.indexedDB.databases) {
+            window.indexedDB.databases = function() {
+                return new Promise((resolve) => {
+                    var req = window.indexedDB.open("dummy");
+                    req.onsuccess = function() {
+                        req.result.close();
+                        resolve(
+                            Array.from(window.indexedDB.databases || []).map(db => ({
+                                name: db.name,
+                                version: db.version
+                            }))
+                        );
+                    };
+                    req.onerror = function() {
+                        resolve([]);
+                    };
+                });
+            };
+        }
+        
         e.setState({ isReady: true });
-      };
-      
-      request.onerror = function(event) {
-        console.error("IndexedDB error:", event.target.error);
-      };
-    }
-  }]), n
-}(i.Component);
-
-// Modify the API methods to work directly with IndexedDB
-const apiMethods = {
-  init: function() {
-    return { apiInitialized: true, apiSignedIn: true };
-  },
-  hasSignedIn: function() {
-    return true;
-  },
-  getResourceById: function(e, t) {
-    return new Promise((resolve) => {
-      const transaction = db.transaction(['files'], 'readonly');
-      const store = transaction.objectStore('files');
-      const request = store.get(t);
-      
-      request.onsuccess = function() {
-        resolve(request.result || null);
-      };
+        window.fs = window.require("fs");
     });
-  },
-  getChildrenForId: function(e, t) {
-    return new Promise((resolve) => {
-      const transaction = db.transaction(['files'], 'readonly');
-      const store = transaction.objectStore('files');
-      const request = store.getAll();
-      
-      request.onsuccess = function() {
-        const allFiles = request.result || [];
-        // Filter files where parentId matches the requested ID
-        const children = allFiles.filter(file => file.parentId === t);
-        resolve(children);
-      };
-    });
-  },
-  // Add other necessary API methods...
-};
-
-// Update the default export to use our modified API
-g.a.api = apiMethods;
-
-o.a.render(
-  Object(a.jsx)(r.a.StrictMode, { children: Object(a.jsx)(FileManagerComponent, {}) }),
-  document.getElementById("root")
-);
+}
